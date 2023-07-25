55ef1f713a1bd2e4ec09ac8562c2c976e3b5e28a
Arnaud <flatsko@gmail.com>
2023-07-25 23:44:01 +0200

    🚀 FP02: APP ADD ROUTES

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import LoginPage from "./component/pages/login/LoginPage.jsx";
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+//import mainLayout from "./component/pages/layout/mainLayout.jsx";
+import OrderPage from "./component/pages/login/OrderPage.jsx";
 
 function App() {
   //state
@@ -7,7 +10,19 @@ function App() {
   //comportement
 
   //affichage
-  return   <LoginPage />
+
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="loginPage" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+
 
 }
 export default App
