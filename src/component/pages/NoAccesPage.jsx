201e2c1ba2ba69ce285677c86f22ba60a2679572
Arnaud <flatsko@gmail.com>
2023-07-26 21:16:53 +0200

    🎨 F02: Improve Button HOME

--- src/component/pages/NoAccesPage.jsx
+++ src/component/pages/NoAccesPage.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { redirect, useNavigate } from 'react-router-dom'
+import { redirect, useNavigate, Link } from 'react-router-dom'
 
 export default function NoAccessPage() {
-    const nav = useNavigate();
+  //  const nav = useNavigate();
 
-    const handleClick = (e) => {
-        console.log(e.preventDefault);
-        e.preventDefault();
-        console.log(e.preventDefault);
-        nav("/");
-    }
+   // const handleClick = (e) => {
+   //    console.log(e.preventDefault);
+   //     e.preventDefault();
+   //     console.log(e.preventDefault);
+  //      nav("/");
+  //  }
   return (
     <div>Rien à voir ici
-
-    <button onClick={handleClick}>Retour à l'accueil</button>
-    
+<Link to="/">
+    <button>Retour à l'accueil</button>
+    </Link>
     </div>
   )
 }
