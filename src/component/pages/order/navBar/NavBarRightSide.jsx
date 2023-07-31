b806effe12588124b99cc6eef6b5177c7e4820e0
Arnaud <flatsko@gmail.com>
2023-07-31 21:26:05 +0200

    ✨ F06 : NavBarRightSide ADD Admin Button

--- src/component/pages/order/navBar/NavBarRightSide.jsx
+++ src/component/pages/order/navBar/NavBarRightSide.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 import { theme } from "../../../../theme/index";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusableUI/ToggleButton";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function NavBarRightSide({ username }) {
+  const [isChecked, setIsChecked] = useState("false");
+  const handleToggle = () => {
+    if (isChecked) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+
+    setIsChecked(!isChecked);
+  };
+
   return (
     <NavBarRightSideStyles>
-      <ToggleButton />
+      <i className="toggle">
+        <ToggleButton
+          onToggle={handleToggle}
+          labelIfChecked="ACTIVER LE MODE ADMIN"
+          labelIfUnchecked="DÉSACTIVER LE MODE ADMIN"
+          isChecked={isChecked}
+        />
+      </i>
       {/* <div className="adminButton">Admin Button</div> */}
       <Profile username={username} />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
     </NavBarRightSideStyles>
   );
 }
@@ -18,4 +47,13 @@ const NavBarRightSideStyles = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
+
+  .toggle {
+    margin-right: 50px;
+  }
+
+
+    div {
+      line-height: 1.3em;
+    }
 `;
