e5746f5de90bfcf24b014dfadcdfcdf030a23150
Arnaud <flatsko@gmail.com>
2023-07-31 21:06:52 +0200

    ✨ F06: ADD Toastify and ToggleButton

--- src/component/pages/order/navBar/NavBarRightSide.jsx
+++ src/component/pages/order/navBar/NavBarRightSide.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 import { theme } from "../../../../theme/index";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusableUI/ToggleButton";
 
 export default function NavBarRightSide({ username }) {
   return (
     <NavBarRightSideStyles>
+      <ToggleButton />
       {/* <div className="adminButton">Admin Button</div> */}
-     <Profile username={username} />
-
+      <Profile username={username} />
     </NavBarRightSideStyles>
   );
 }
@@ -18,5 +18,4 @@ const NavBarRightSideStyles = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
-    
 `;
