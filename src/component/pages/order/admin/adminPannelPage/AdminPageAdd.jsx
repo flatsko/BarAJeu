2a12419fe06ea56754c68b5546fbbd4c7287b1fc
Arnaud <flatsko@gmail.com>
2023-09-12 23:52:40 +0200

    🚑 F08 Update Admin Page

--- src/component/pages/order/admin/adminPannelPage/AdminPageAdd.jsx
+++ src/component/pages/order/admin/adminPannelPage/AdminPageAdd.jsx
@@ -11,21 +11,41 @@ export default function AdminPageAdd() {
     const [nom, setNom] = useState("Nom");
     const [image, setImage] = useState("Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)");
     const [prix, setPrix] = useState("Prix");
-    const [urlImage, setUrlImage] = useState();
+    const [urlImage, setUrlImage] = useState("https://www.laboutiqueharibo.fr/dw/image/v2/BFFT_PRD/on/demandware.static/-/Sites-haribo-master-catalog/default/dw809ff44e/food/AP/102896-Dentier-210-bonbons.png?sw=460&sh=460");
 
-    const handleChange = (e) => {
+    const handleChangeNom = (e) => {
         setNom(e.target.value);
         console.log(e);
 
     }
+    const handleChangeImage = (e) => {
+        setImage(e.target.value);
+        console.log(e);
 
+    }
+    const handleChangePrix = (e) => {
+        setPrix(e.target.value);
+        console.log(e);
+
+    }
+
+    const handleAdd = (e) => {
+        e.preventdefault();
+
+        const newProduct = 
+        {
+            
+        }
+    }
   return (
     <AdminPageAddStyled> 
-        <img className='a' src='https://www.laboutiqueharibo.fr/dw/image/v2/BFFT_PRD/on/demandware.static/-/Sites-haribo-master-catalog/default/dwadacee4e/food/summer%20pinata%20party/106109_HD.png?sw=460&sh=460'></img>
-     
-        <TextInput className={"b"} Icon={<FaHamburger />} name={nom} onChange={handleChange}></TextInput>
-        <TextInput className={"c"} Icon={<BsFillCameraFill />} name={image} onChange={handleChange}></TextInput>
-        <TextInput className={"d"} Icon={<MdOutlineEuro />} name={prix} onChange={handleChange}></TextInput>
+        <img className='a' src={urlImage}></img>
+        
+        <TextInput className={"b"} Icon={<FaHamburger />} name={nom} onChange={handleChangeNom}></TextInput>
+        <TextInput className={"c"} Icon={<BsFillCameraFill />} name={image} onChange={handleChangeImage}></TextInput>
+        <TextInput className={"d"} Icon={<MdOutlineEuro />} name={prix} onChange={handleChangePrix}></TextInput>
+        
+        
         <PrimaryButton className={"e"} icon={MdOutlineEuro} label={"Ajouter un nouveau produit au menu"} />
         
     </AdminPageAddStyled>
@@ -35,12 +55,14 @@ export default function AdminPageAdd() {
 
 const AdminPageAddStyled = styled.div`
   display:grid;
-  grid-template-columns: 20% 80%;
-  grid-template-rows: repeat(5, 20%);
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 20%);
+  height: 100%;
+  width:70%;
   .a{
-    grid-column-start: 1;
-    grid-row: 1/5;
-    max-width: 70%;
+    grid-area: 1 / 1 / 4 / 2;
+    max-width: 100%;
+
   }
   .b{
     grid-column-start: 2;
@@ -55,10 +77,10 @@ const AdminPageAddStyled = styled.div`
     grid-row-start: 3;
   }
   .e{
-    grid-column-start: 2;
-    grid-row-start: 4;
+    grid-area: 4 / 2 / -2 / -1;
     color: ${theme.colors.white};
     background-color: ${theme.colors.green};
+    width: 50%;
     
     &:hover
     {
